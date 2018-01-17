4aafeb79b9f69f51efbad64c7c59bdcb2029ef9c
josh-alamillo7 <josh.alamillo7@gmail.com>
2018-01-17 15:10:56 -0800

    through functional structures

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,9 @@
 var Queue = function() {
-  var someInstance = {};
+  var someInstance = {
+    queuelength: 0
+  };
+
+
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +11,26 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance.queuelength += 1;
+    storage[someInstance.queuelength] = value;
   };
 
   someInstance.dequeue = function() {
+    var currentQueue = someInstance.queuelength;
+    var removedValue = storage[1];
+
+    if (currentQueue > 0) {
+      for (i = 0; i < currentQueue; i++) {
+        storage[i] = storage[i + 1];  
+      }
+      delete storage[currentQueue];
+      someInstance.queuelength -= 1;
+    }
+    return removedValue;
   };
 
   someInstance.size = function() {
+    return someInstance.queuelength;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,35 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+    stackheight: 0,
+};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+  
+  someInstance.stackheight += 1;
+  storage[someInstance.stackheight] = value;
+
   };
 
   someInstance.pop = function() {
+
+  var currentStack = someInstance.stackheight;
+  var removedValue = storage[currentStack];
+  if (currentStack > 0) {
+    delete storage[currentStack];
+    someInstance.stackheight -= 1;
+  }
+
+  return removedValue;
+  
+
   };
 
   someInstance.size = function() {
+  return someInstance.stackheight;
   };
 
   return someInstance;
