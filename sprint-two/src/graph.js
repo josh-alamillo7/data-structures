4becaebf45046befc0d8e9892336d33fd31da84c
josh-alamillo7 <josh.alamillo7@gmail.com>
2018-01-18 18:59:34 -0800

    finished graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -38,6 +38,11 @@ Graph.prototype.removeNode = function(nodeValue) {
   var removedNodeIndex = this.findNodeIndex(nodeValue, this.nodes);
   var removedNode = this.nodes[removedNodeIndex];
   
+  for (i = 0; i < removedNode.edges.length; i++) {
+    var edgeObjectValue = removedNode.edges[i].value;
+    this.removeEdge(nodeValue, edgeObjectValue);
+  }
+  
   this.nodes.splice(removedNodeIndex, 1);
   
 };
@@ -47,7 +52,7 @@ Graph.prototype.hasEdge = function(fromNodeValue, toNodeValue) {
   var fromNodeIndex = this.findNodeIndex(fromNodeValue, this.nodes);
   var fromNode = this.nodes[fromNodeIndex];
   
-  if(typeof this.findNodeIndex(toNodeValue, fromNode.edges) === 'number') {
+  if (typeof this.findNodeIndex(toNodeValue, fromNode.edges) === 'number') {
     return true;
   }
   return false;
@@ -67,14 +72,43 @@ Graph.prototype.addEdge = function(fromNodeValue, toNodeValue) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNodeValue, toNodeValue) {
+  var fromNodeIndex = this.findNodeIndex(fromNodeValue, this.nodes);
+  var fromNode = this.nodes[fromNodeIndex];
+  
+  var toNodeIndex = this.findNodeIndex(toNodeValue, this.nodes);
+  var toNode = this.nodes[toNodeIndex];
+  
+  var toNodeEdgeIndex = this.findNodeIndex(toNodeValue, fromNode.edges);
+  var fromNodeEdgeIndex = this.findNodeIndex(fromNodeValue, toNode.edges);
+  
+  fromNode.edges.splice(toNodeEdgeIndex, 1); 
+  toNode.edges.splice(fromNodeEdgeIndex, 1);
+  
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (i = 0; i < this.nodes.length; i++) {
+    cb(this.nodes[i].value);
+    
+  }
+  
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ Graph: constant
+ addNode: constant
+ contains: linear
+ findNodeIndex: linear
+ removeNode: quadratic
+ hasEdge: linear
+ addEdge:linear
+ removeEdge: linear
+ forEachNode: linear (depending on what function is passed in,
+              but the base is linear: O(n * run time of cb))
+ 
+ 
  */
 
 
