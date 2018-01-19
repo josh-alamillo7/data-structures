bba579b7174a2b3ffe2c2b143208d87b9f62c45b
josh-alamillo7 <josh.alamillo7@gmail.com>
2018-01-19 12:30:35 -0800

    passing all tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,65 @@
 var BinarySearchTree = function(value) {
+  var newTree = {
+    value: value,
+    left: null,
+    right: null,
+  };
+  _.extend(newTree, BinarySearchTreeMethods);
+  return newTree;
+};
+
+BinarySearchTreeMethods = {};
+
+BinarySearchTreeMethods.traverse = function (value) {
+  
+};
+
+BinarySearchTreeMethods.insert = function (value) {
+  if (this.value > value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value); 
+    } else {
+      this.left.insert(value);
+    }
+    
+  } else if (this.value < value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    } 
+  }
+};
+
+BinarySearchTreeMethods.contains = function (value) {
+  if (this.value === value) {
+    return true;
+  }
+  if (this.value > value) {
+    if (this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  } else if (this.value < value) {
+    if (this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  }
+  
+};
+
+BinarySearchTreeMethods.depthFirstLog = function (cb) {
+  cb(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(cb);
+  }
+  
 };
 
 
