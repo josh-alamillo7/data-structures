3bb60c0ce9031f394506af5a82c9b729bc066b9b
josh-alamillo7 <josh.alamillo7@gmail.com>
2018-01-18 19:15:30 -0800

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  var itemIndex = this._storage.indexOf(item);
+  this._storage.splice(itemIndex, 1);
+  return item;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ Set: constant
+ add: linear
+ contains: linear
+ remove: linear
  */
