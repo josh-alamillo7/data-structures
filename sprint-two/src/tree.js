2b0628b1ec213a661f661b3cfb8d55d368b90af4
josh-alamillo7 <josh.alamillo7@gmail.com>
2018-01-18 16:39:48 -0800

    solved tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,39 @@
 var Tree = function(value) {
   var newTree = {};
+  
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; 
+  _.extend(newTree, treeMethods);
+   
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChildTree = Tree(value);
+  this.children.push(newChildTree);
 };
 
 treeMethods.contains = function(target) {
+  //debugger;
+  var searchAllTrees = function (tree) {
+    if (tree.value === target) {
+      return true;
+    }
+    
+    if (tree.children.length === 0) {
+      return false;
+      
+    } else {
+      return tree.children.reduce( function (acc, child) {
+        return acc || searchAllTrees(child);
+      }, false);
+    }
+    
+  };
+  
+  return searchAllTrees(this);
 };
 
 
