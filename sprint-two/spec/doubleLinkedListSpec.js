3d2095e7fa551aed2e8cf96612b55773b724ef49
josh-alamillo7 <josh.alamillo7@gmail.com>
2018-01-20 10:33:59 -0800

    added double linked lists w/ all passing tests

--- sprint-two/spec/doubleLinkedListSpec.js
+++ sprint-two/spec/doubleLinkedListSpec.js
@@ -10,6 +10,14 @@ describe('doubleLinkedList', function() {
     expect(doubleLinkedList).to.have.property('tail');
     expect(doubleLinkedList).to.have.property('length');
   });
+
+  it('should have methods "addToHead", "addToTail", "removeHead", "removeTail", and "contains"', function () {
+    expect(doubleLinkedList.addToHead).to.be.a('function');
+    expect(doubleLinkedList.addToTail).to.be.a('function');
+    expect(doubleLinkedList.removeHead).to.be.a('function');
+    expect(doubleLinkedList.removeTail).to.be.a('function');
+    expect(doubleLinkedList.contains).to.be.a('function');
+  });
   
   it('should have nodes containing a next and prev value', function() {
     doubleLinkedList.addToTail(6);
@@ -18,48 +26,98 @@ describe('doubleLinkedList', function() {
     expect(doubleLinkedList.tail).to.have.property('prev');
   });
   
-  it('should contain a node that has been added', function () {
-    
+  it('should contain a node that has been added to the tail', function () {
+    doubleLinkedList.addToTail(4);
+    expect(doubleLinkedList.contains(4)).to.equal(true);
+  });
+  
+  it('should contain a node that has been added to the head', function() {
+    doubleLinkedList.addToHead(3);
+    expect(doubleLinkedList.contains(3)).to.equal(true);
   });
   
   it('should designate a new tail when a node has been added to the tail', function () {
-    
+    doubleLinkedList.addToHead(3);
+    doubleLinkedList.addToTail(7);
+    doubleLinkedList.addToTail(8);
+    expect(doubleLinkedList.tail.value).to.equal(8);
   });
   
   it('should designate a new head when a node has been added to the head', function () {
-    
+    doubleLinkedList.addToHead(5);
+    doubleLinkedList.addToHead(6);
+    expect(doubleLinkedList.head.value).to.equal(6);
   });
   
-  it('should not contain a node that has been removed', function () {
-    
+  it('should not contain a node that has been removed from the head', function () {
+    doubleLinkedList.addToHead(4);
+    doubleLinkedList.addToTail(5);
+    doubleLinkedList.removeHead();
+    expect(doubleLinkedList.contains(4)).to.equal(false);
+  });
+  
+  it('should not contain a node that has been removed from the tail', function () {
+    doubleLinkedList.addToTail(5);
+    doubleLinkedList.removeTail();
+    expect(doubleLinkedList.contains(5)).to.equal(false);
   });
   
   it('should designate a new tail when a node has been removed from the tail', function () {
-    
+    doubleLinkedList.addToHead(3);
+    doubleLinkedList.addToTail(5);
+    doubleLinkedList.addToTail(4);
+    doubleLinkedList.removeTail();
+    expect(doubleLinkedList.tail.value).to.equal(5);
   });
   
   it('should designate a new head when a node has been removed from the head', function () {
-    
+    doubleLinkedList.addToHead(3);
+    doubleLinkedList.addToTail(5);
+    doubleLinkedList.addToTail(4);
+    doubleLinkedList.removeHead();
+    expect(doubleLinkedList.head.value).to.equal(5);
+  });
+  
+  it('should return the value of the removed node', function () {
+    doubleLinkedList.addToHead(1);
+    doubleLinkedList.addToTail(2);
+    doubleLinkedList.addToTail(3);
+    expect(doubleLinkedList.removeHead()).to.equal(1);
+    expect(doubleLinkedList.removeTail()).to.equal(3);
   });
   
   it('should have an increased length property when a node is added', function () {
-    
+    doubleLinkedList.addToTail(3);
+    expect(doubleLinkedList.length).to.equal(1);
   });
   
   it('should have a decreased length property when a node is removed', function () {
-    
+    doubleLinkedList.addToHead(9);
+    doubleLinkedList.addToHead(8);
+    doubleLinkedList.removeTail();
+    expect(doubleLinkedList.length).to.equal(1);
   });
   
   it('should insert nodes at the correct place in the list', function () {
-    
+    doubleLinkedList.addToHead(4);
+    doubleLinkedList.addToTail(6);
+    doubleLinkedList.insert(4, 5);
+    expect(doubleLinkedList.head.next.value).to.equal(5);
+    expect(doubleLinkedList.tail.prev.value).to.equal(5);
   });
   
   it('should be able to traverse the list from head to tail after a node has been inserted', function () {
-    
+    doubleLinkedList.addToHead(2);
+    doubleLinkedList.addToTail(4);
+    doubleLinkedList.insert(2, 3);
+    expect(doubleLinkedList.traverseFromHead(4).value).to.equal(4);
   });
   
   it('should be able to traverse the list from tail to head after a node has been inserted', function () {
-    
+    doubleLinkedList.addToHead(2);
+    doubleLinkedList.addToTail(4);
+    doubleLinkedList.insert(2, 3);
+    expect(doubleLinkedList.traverseFromTail(2).value).to.equal(2);
   });
   
 });
--- sprint-two/src/doubleLinkedList.js
+++ sprint-two/src/doubleLinkedList.js
@@ -5,31 +5,118 @@ var DoubleLinkedList = function () {
   doubleList.length = 0;
   
   doubleList.addToTail = function (value) {
-    
+    var newNode = DoubleNode(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
   };
   
   doubleList.addToHead = function (value) {
-    
+    var newNode = DoubleNode(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+      this.length++;
+    }
   };
   
   doubleList.removeTail = function () {
-    
+    if (this.length > 1) {
+      removedNodeValue = this.tail.value;
+      this.tail = this.tail.prev;
+      delete this.tail.next;
+      this.tail.next = null;
+      this.length--;
+      return removedNodeValue;
+    } else if (this.length === 1) {
+      removedNodeValue = this.tail.value;
+      delete this.tail;
+      this.tail = null;
+      this.head = null;
+      this.length--;
+      return removedNodeValue;
+    }
   };
   
   doubleList.removeHead = function () {
-    
+    if (this.length > 1) {
+      removedNodeValue = this.head.value;
+      this.head = this.head.next;
+      delete this.head.prev;
+      this.head.prev = null;
+      this.length--;
+      return removedNodeValue;
+    } else if (this.length === 1) {
+      removedNodeValue = this.head.value;
+      delete this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return removedNodeValue;
+    }
   };
   
-  doubleList.traverse = function () {
+  doubleList.traverseFromHead = function (target) {
+    if (this.length === 0) {
+      return undefined;
+    }
     
+    var findNode = function (startingNode) {
+      if (startingNode.value === target) {
+        return startingNode;
+      } else if (startingNode.next === null) {
+        return undefined;
+      } else {
+        return findNode(startingNode.next);
+      }
+    };
+    
+    return findNode(this.head);
+  };
+  
+  doubleList.traverseFromTail = function (target) {
+    if (this.length === 0) {
+      return undefined;
+    }
+    
+    var findNode = function (startingNode) {
+      if (startingNode.value === target) {
+        return startingNode;
+      } else if (startingNode.prev === null) {
+        return undefined;
+      } else {
+        return findNode(startingNode.prev);
+      }
+    };
+    
+    return findNode(this.tail);
   };
   
   doubleList.contains = function (value) {
-    
+    return !!this.traverseFromHead(value);
   };
   
   doubleList.insert = function (target, value) {
+    var newNode = DoubleNode(value);
+    var targetNode = this.traverseFromHead(target);
     
+    newNode.prev = targetNode;
+    newNode.next = targetNode.next;
+    
+    targetNode.next.prev = newNode;
+    targetNode.next = newNode;
   };
   
   return doubleList;
@@ -40,4 +127,5 @@ var DoubleNode = function (value) {
   _node.value = value;
   _node.next = null;
   _node.prev = null;
+  return _node;
 };
